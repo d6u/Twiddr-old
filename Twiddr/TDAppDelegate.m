274e483b8c8f6481df273561311cf15122867054
Daiwei Lu <daiweilu123@gmail.com>
2014-04-22 21:12:38 -0400

    Create singleton manager of core data context

--- Twiddr/TDAppDelegate.m
+++ Twiddr/TDAppDelegate.m
@@ -8,14 +8,10 @@
 
 #import "TDAppDelegate.h"
 #import "TDAccountTableViewController.h"
+#import "TDSingletonCoreDataManager.h"
 
 
 @implementation TDAppDelegate
-
-
-@synthesize managedObjectContext = _managedObjectContext;
-@synthesize managedObjectModel = _managedObjectModel;
-@synthesize persistentStoreCoordinator = _persistentStoreCoordinator;
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -23,7 +19,7 @@
     // Assign ManagedObjectContext to first view controller
     UINavigationController *navigationController = (UINavigationController *)self.window.rootViewController;
     TDAccountTableViewController *controller = (TDAccountTableViewController *)navigationController.topViewController;
-    controller.managedObjectContext = [self managedObjectContext];
+    controller.managedObjectContext = [TDSingletonCoreDataManager getManagedObjectContext];
     
     return YES;
 }
@@ -51,92 +47,7 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
-    [self saveContext];
-}
-
-
-- (void)saveContext
-{
-    NSError *error = nil;
-    NSManagedObjectContext *managedObjectContext = self.managedObjectContext;
-    if (managedObjectContext != nil) {
-        if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
-            // TODO: Replace
-            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-            abort();
-        } 
-    }
-}
-
-
-#pragma mark - Core Data stack
-
-- (NSManagedObjectContext *)managedObjectContext
-{
-    if (_managedObjectContext != nil) {
-        return _managedObjectContext;
-    }
-    
-    NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
-    if (coordinator != nil) {
-        _managedObjectContext = [[NSManagedObjectContext alloc] init];
-        [_managedObjectContext setPersistentStoreCoordinator:coordinator];
-    }
-    return _managedObjectContext;
-}
-
-
-- (NSManagedObjectModel *)managedObjectModel
-{
-    if (_managedObjectModel != nil) {
-        return _managedObjectModel;
-    }
-    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"Twiddr" withExtension:@"momd"];
-    _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
-    return _managedObjectModel;
-}
-
-
-- (NSPersistentStoreCoordinator *)persistentStoreCoordinator
-{
-    if (_persistentStoreCoordinator != nil) {
-        return _persistentStoreCoordinator;
-    }
-    
-    NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"Twiddr.sqlite"];
-    
-    NSError *error = nil;
-    _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc]
-                                   initWithManagedObjectModel:[self managedObjectModel]];
-    if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType
-                                                   configuration:nil URL:storeURL options:nil error:&error])
-    {
-        /*
-         * If you encounter schema incompatibility errors during development, you can reduce their frequency by:
-         * Simply deleting the existing store:
-         * [[NSFileManager defaultManager] removeItemAtURL:storeURL error:nil]
-         
-         * Performing automatic lightweight migration by passing the following dictionary as the options parameter:
-         * @{NSMigratePersistentStoresAutomaticallyOption:@YES, NSInferMappingModelAutomaticallyOption:@YES}
-         
-         * Lightweight migration will only work for a limited set of schema changes; 
-         * consult "Core Data Model Versioning and Data Migration Programming Guide" for details.
-         */
-        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-        abort();
-    }    
-    
-    return _persistentStoreCoordinator;
-}
-
-
-#pragma mark - Application's Documents directory
-
-// Returns the URL to the application's Documents directory.
-- (NSURL *)applicationDocumentsDirectory
-{
-    return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory
-                                                   inDomains:NSUserDomainMask] lastObject];
+    [TDSingletonCoreDataManager saveContext];
 }
 
 
---
+++ Twiddr/TDSingletonCoreDataManager.m
@@ -0,0 +1,125 @@
+//
+//  TDManagedContext.m
+//  Twiddr
+//
+//  Created by Daiwei Lu on 4/22/14.
+//  Copyright (c) 2014 Daiwei Lu. All rights reserved.
+//
+
+#import "TDSingletonCoreDataManager.h"
+
+
+@interface TDSingletonCoreDataManager () {
+    NSManagedObjectContext *_managedObjectContext;
+    NSManagedObjectModel *_managedObjectModel;
+    NSPersistentStoreCoordinator *_persistentStoreCoordinator;
+}
+@end
+
+
+@implementation TDSingletonCoreDataManager
+
+static TDSingletonCoreDataManager *_coreDataManager;
+
++ (instancetype)sharedCoreDataManager
+{
+    if (_coreDataManager == nil) {
+        _coreDataManager = [[super alloc] init];
+    }
+    return _coreDataManager;
+}
+
+
++ (NSManagedObjectContext *)getManagedObjectContext
+{
+    TDSingletonCoreDataManager *coreDataManager = [TDSingletonCoreDataManager sharedCoreDataManager];
+    return [coreDataManager managedObjectContext];
+}
+
+
++ (void)saveContext
+{
+    NSError *error = nil;
+    NSManagedObjectContext *managedObjectContext = [TDSingletonCoreDataManager getManagedObjectContext];
+    if (managedObjectContext != nil) {
+        if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
+            // TODO: Replace
+            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+            abort();
+        }
+    }
+}
+
+
+#pragma mark - Core Data stack
+
+- (NSManagedObjectContext *)managedObjectContext
+{
+    if (_managedObjectContext != nil) {
+        return _managedObjectContext;
+    }
+    
+    NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
+    if (coordinator != nil) {
+        _managedObjectContext = [[NSManagedObjectContext alloc] init];
+        [_managedObjectContext setPersistentStoreCoordinator:coordinator];
+    }
+    return _managedObjectContext;
+}
+
+
+- (NSManagedObjectModel *)managedObjectModel
+{
+    if (_managedObjectModel != nil) {
+        return _managedObjectModel;
+    }
+    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"Twiddr" withExtension:@"momd"];
+    _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
+    return _managedObjectModel;
+}
+
+
+- (NSPersistentStoreCoordinator *)persistentStoreCoordinator
+{
+    if (_persistentStoreCoordinator != nil) {
+        return _persistentStoreCoordinator;
+    }
+    
+    NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"Twiddr.sqlite"];
+    
+    NSError *error = nil;
+    _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc]
+                                   initWithManagedObjectModel:[self managedObjectModel]];
+    if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType
+                                                   configuration:nil URL:storeURL options:nil error:&error])
+    {
+        /*
+         * Performing automatic lightweight migration by passing the following dictionary as the options parameter:
+         * @{NSMigratePersistentStoresAutomaticallyOption:@YES, NSInferMappingModelAutomaticallyOption:@YES}
+         
+         * Lightweight migration will only work for a limited set of schema changes;
+         * consult "Core Data Model Versioning and Data Migration Programming Guide" for details.
+         */
+        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+        
+        // TODO: remove in production
+        [[NSFileManager defaultManager] removeItemAtURL:storeURL error:nil];
+        
+        return [self persistentStoreCoordinator];
+    }
+    
+    return _persistentStoreCoordinator;
+}
+
+
+#pragma mark - Application's Documents directory
+
+// Returns the URL to the application's Documents directory.
+- (NSURL *)applicationDocumentsDirectory
+{
+    return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory
+                                                   inDomains:NSUserDomainMask] lastObject];
+}
+
+
+@end
